4ebc2b8f299db28941c8a84703e62f520d8042ed
rag-nath <rnarwar@protonmail.com>
2022-08-06 22:15:29 +0530

    update

--- Cute magic.jl
+++ Cute magic.jl
@@ -204,8 +204,86 @@ md"""
 ---
 """
 
-# ╔═╡ 2cb755ea-9359-4c74-bea5-9994e0e1c80c
+# ╔═╡ d9833230-4d7f-483a-9693-e13d0bc372de
+begin
+	function optred(v::Vector)
+		for i in 2:length(v) #range(1, len(v))
+			if v[i-1] > v[i]
+				return false
+			end
+		end
+		return true
+	end
+	v = [collect(1:10000); 1]#; 100]
+	optred(v)
+end
 
+# ╔═╡ 9076d9bd-b5a2-4ef4-8a72-4ee914a46bf9
+md"""
+---
+---
+"""
+
+# ╔═╡ aaad674f-233a-4802-b57a-da6132b9fb44
+md"""
+A 0-indexed array a of size n is called good if for all valid indices i (0≤i≤n−1), ai+i is a perfect square†
+
+.
+
+Given an integer n
+. Find a permutation‡ p of [0,1,2,…,n−1]
+
+that is good or determine that no such permutation exists.
+
+†
+An integer x is said to be a perfect square if there exists an integer y such that x=y2
+
+.
+
+‡
+An array b is a permutation of an array a if b consists of the elements of a in arbitrary order. For example, [4,2,3,4] is a permutation of [3,2,4,4] while [1,2,2] is not a permutation of [1,2,3]
+
+.
+Input
+
+The first line contains a single integer t
+(1≤t≤104
+
+) — the number of test cases.
+
+The only line of each test case contains a single integer n
+(1≤n≤105) — the length of the permutation p
+
+.
+
+It is guaranteed that the sum of n
+over all test cases does not exceed 105
+
+.
+Output
+
+For each test case, output n
+distinct integers p0,p1,…,pn−1 (0≤pi≤n−1) — the permutation p — if the answer exists, and −1 otherwise.
+"""
+
+# ╔═╡ 441a45bf-b2ab-421f-9e40-ff6aeac75c99
+function goodarray(n)
+	for i = 0:n-1
+		p = floor(Int, √(i))
+		qpossible = Vector{Int}(undef,0)
+		
+		for q = 0:n-1
+			r² = (p + q)^2
+			if (r² - i >= 0 && r² - i < n-1)
+				push!(qpossible, r² - i)
+			end
+		end
+		println("$i $qpossible")
+	end
+end
+
+# ╔═╡ 2adeb08d-0a36-4af0-9aa4-b99abd6258c4
+goodarray(17)
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -239,6 +317,10 @@ manifest_format = "2.0"
 # ╟─65cf904a-5f81-42b2-b9fd-769a92bf350e
 # ╠═d1ec8bc8-8361-46e9-872d-1ef1ce5f1828
 # ╟─9d62d103-10bb-43a6-9188-133a20636470
-# ╠═2cb755ea-9359-4c74-bea5-9994e0e1c80c
+# ╠═d9833230-4d7f-483a-9693-e13d0bc372de
+# ╟─9076d9bd-b5a2-4ef4-8a72-4ee914a46bf9
+# ╟─aaad674f-233a-4802-b57a-da6132b9fb44
+# ╠═441a45bf-b2ab-421f-9e40-ff6aeac75c99
+# ╠═2adeb08d-0a36-4af0-9aa4-b99abd6258c4
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
